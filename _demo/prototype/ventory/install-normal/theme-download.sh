49ef5f2ee46546b6d1c7c5c85868b8f09b0dbca8
samwhelp <samwhelp@users.noreply.github.com>
2022-05-02 10:55:08 +0800

    fix

---
+++ _demo/prototype/ventory/install-normal/theme-download.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/Teraskull/bigsur-grub2-theme.git
+
---
+++ _demo/prototype/ventory/install-normal/theme-install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+#cp bigsur-grub2-theme/ventoy /media/$(id -un)/Ventoy -a
+
+cp bigsur-grub2-theme/ventoy /var/run/media/$(id -un)/Ventoy -a
